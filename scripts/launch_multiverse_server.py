83efa8d213e65c265b88384813a2f82e39c744aa
Giang Nguyen <hoanggia@uni-bremen.de>
2025-04-24 16:28:19 +0200

    Add files

---
+++ scripts/launch_multiverse_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from multiverse_launch import MultiverseLaunch
+from utils import run_subprocess
+import os
+
+
+def main():
+    multiverse_launch = MultiverseLaunch()
+
+    server_port = multiverse_launch.multiverse_server["port"]
+    multiverse_launch_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+    multiverse_server_path = os.path.join(multiverse_launch_path, "bin", "multiverse_server")
+    run_subprocess([multiverse_server_path, f"tcp://*:{server_port}"])
+
+
+if __name__ == "__main__":
+    main()
